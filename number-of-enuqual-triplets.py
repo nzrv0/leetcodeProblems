557565e4093fd17d6d21040c68fb61e8a75dcfe7
nazarov <nezerovabuzar@gmail.com>
2024-12-05 20:14:36 +0400

    latest code

--- number-of-enuqual-triplets.py
+++ number-of-enuqual-triplets.py
@@ -1,34 +1,20 @@
+from collections import Counter
+
+
 def uniqual(nums):
-    nums.sort()
-    unice = list(set(nums))
-    ex = {}
-    ss = []
+    c = Counter(nums)
 
-    for num in nums:
-        ex[num] = 1 + ex.get(num, 0)
-    for item in ex.keys():
-        if ex[max(ex.keys())] != 1:
-            ss.append(item)
+    res = 0
+    left = 0
+    right = len(nums)
 
-        ex[item] -= 1
-    print(ss)
-    return len(ss)
-    # res = 0
-    # if len(unice) <= 1:
-    #     return 0
-
-    # if max(nums) > len(nums):
-    #     unice.pop()
-
-    # if len(unice) == 2:
-    #     return 1
-
-    # while res < min(nums) and min(nums) >= 2:
-    #     res += 1
-
-    # return res + 1
+    for _, freq in c.items():
+        right -= freq
+        res += left * freq * right
+        left += freq
+    return res
 
 
-nums = [1000, 999, 998]
+nums = [1, 1, 1]
 resoult = uniqual(nums)
 print(resoult)
