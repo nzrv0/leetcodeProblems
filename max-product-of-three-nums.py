1d7631d98e297bd094549c6ecdad6611959b9cb4
nazarov <nezerovabuzar@gmail.com>
2024-12-07 18:24:44 +0400

    code added

--- max-product-of-three-nums.py
+++ max-product-of-three-nums.py
@@ -1,26 +1,7 @@
 def product_nums(nums):
-    # nn = sorted(list(map(lambda x: x * -1 if x < 0 else x, nums)), reverse=True)
-
-    # res = 1
-    # nums.sort()
-    # print(nums)
-    # ll = map(lambda x: -1 if x < 0 else 1, nums)
-    # for i in nn[:3]:
-    #     res *= i
-    #     print(i)
-    mm = 0
-    ll = []
-    res = 1
-    for num in nums:
-        print(mm, num)
-        if num < 0 and num * -1 > mm:
-            mm = num * -1
-        else:
-            mm = num
-        ll.append(num)
-    for i in ll[::-1][:3]:
-        res *= i
-    print(res)
+    nums.sort()
+    print(nums)
+    return max(nums[-3] * nums[-2] * nums[-1], nums[-1] * nums[0] * nums[1])
 
 
 nums = [-100, -98, -1, 2, 3, 4]
