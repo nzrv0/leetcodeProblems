d645e839ae2c91552d5f9686fb31166e12637540
nazarov <nezerovabuzar@gmail.com>
2024-12-10 22:09:16 +0400

    code added

---
+++ merge-sorted-array.py
@@ -0,0 +1,11 @@
+def merge_sorted(nums1, nums2, m, n):
+    nums1[m:] = nums2
+    nums1.sort()
+    print(nums1)
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+merge_sorted(nums1, nums2, m, n)
