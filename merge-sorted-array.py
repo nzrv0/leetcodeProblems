455130689357a2e6a3748b324e4c15732321851c
nazarov <nezerovabuzar@gmail.com>
2024-12-11 19:53:18 +0400

    merge sorted array

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -1,7 +1,17 @@
 def merge_sorted(nums1, nums2, m, n):
-    nums1[m:] = nums2
-    nums1.sort()
-    print(nums1)
+    midx = m - 1
+    nidx = n - 1
+    right = m + n - 1
+
+    while nidx >= 0:
+        if midx >= 0 and nums1[midx] > nums2[nidx]:
+            nums1[right] = nums1[midx]
+            midx -= 1
+
+        else:
+            nums1[right] = nums2[nidx]
+            nidx -= 1
+        right -= 1
 
 
 nums1 = [1, 2, 3, 0, 0, 0]
